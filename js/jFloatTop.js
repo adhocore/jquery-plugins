dc2f37ed52c73f17914dc7f184ddbb4b5975057d
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-08-05 12:23:05 +0700

    Refactor floattop

--- js/jFloatTop.js
+++ js/jFloatTop.js
@@ -4,75 +4,90 @@
  * @author adhocore | Jitendra Adhikari
  * @email jiten.adhikary@gmail.com
  */
-;(function($) {
-  $.fn.jFloatTop= function(options) {
-    options  = $.extend({itemClass: 'box', col: 4,  gap: 4 }, options || {});
-    var wrap = $(this), hh = [], minH = 0, boxes = wrap.find('.'+options.itemClass); box = $(boxes[0]),
-      minW = Math.floor(((int(wrap.width()) - (offsetWidth(wrap) / 2 ) - (options.gap - int(box.css('marginLeft')) - int(box.css('marginRight'))) * (options.col-1)) / options.col) - offsetWidth(box));
+Array.prototype.max = function() {
+  return Math.max.apply(Math, this);
+};
+Array.prototype.min = function() {
+  return Math.min.apply(Math, this);
+};
+Array.prototype.maxi = function() {
+  return this.indexOf(this.max());
+};
+Array.prototype.mini = function() {
+  return this.indexOf(this.min());
+};
 
-    function int(int){
-      return Math.ceil(parseInt(int,10));
+(function($) {
+  'use strict';
+  $.fn.jFloatTop = function(opts) {
+    opts = $.extend({
+      itemClass: 'box',
+      col: 4,
+      gap: 4
+    }, opts || {});
+
+    var wrap = $(this),
+      hh = [],
+      minH = 0,
+      boxes = wrap.find('.' + opts.itemClass),
+      box = $(boxes[0]),
+      minW = Math.floor(
+        (
+          (
+            num(wrap.width()) - (offsetWidth(wrap) / 2) -
+            (opts.gap - num(box.css('marginLeft')) - num(box.css('marginRight')))
+            * (opts.col - 1)
+          ) / opts.col
+        )
+        - offsetWidth(box)
+      );
+
+    function num(n) {
+      return parseInt(n, 10);
     };
 
-    function offsetWidth(el){
-      return ( int(el.css('marginLeft')) + int(el.css('marginRight')) + int(el.css('paddingRight')) + int(el.css('paddingLeft')) + int(el.css('borderLeftWidth')) + int(el.css('borderRightWidth')) );
+    function offsetWidth(el) {
+      return (num(el.css('marginLeft')) + num(el.css('marginRight')) + num(el.css('paddingRight')) + num(el.css('paddingLeft')) + num(el.css('borderLeftWidth')) + num(el.css('borderRightWidth')));
     };
 
     wrap.css('position', 'relative');
 
-    boxes.each(function(i,e){
-
+    boxes.each(function(i, e) {
       $(this).width(minW);
-      h = int($(this).outerHeight());
-      j = i % options.col;
+
+      var h = num($(this).outerHeight());
+      var j = i % opts.col, m = 0;
 
       if (i == 0) {
         m = h;
-        for (k = 0; k < options.col ; k++)
+        for (var k = 0; k < opts.col; k++) {
           hh.push(0);
-      } else m = hh.min();
+        }
+      } else {
+        m = hh.min();
+      }
 
-      minH = (minH == 0) ? m : Math.min(m, minH);
+      minH = minH == 0 ? m : Math.min(m, minH);
 
-      if ( (hh.max() - m) > minH ) {
+      if ((hh.max() - m) > minH) {
         j = hh.mini();
       };
 
       $(this).css({
-          'top':  hh[j],
-          'left': j * ($(this).outerWidth() + options.gap),
-          'position': 'absolute'
-        });
+        top: hh[j],
+        left: j * ($(this).outerWidth() + opts.gap),
+        position: 'absolute'
+      });
 
-      hh[j] += h + options.gap;
-
+      hh[j] += h + opts.gap;
     });
 
-    wrap.height(int(hh.max()));
-    $(window).resize(function(){
-      wrap.floattop(options);
+    wrap.height(num(hh.max()));
+
+    $(window).resize(function() {
+      wrap.floattop(opts);
     });
+
     return wrap; // chaining
   }
 })(jQuery);
-
-/* Array functions */
-
-  Array.prototype.max = function(){
-    return Math.max.apply( Math, this );
-  };
-
-  Array.prototype.min = function(){
-    return Math.min.apply( Math, this );
-  };
-
-  Array.prototype.maxi = function(){
-    return this.indexOf(this.max());
-  };
-
-  Array.prototype.mini = function(){
-    return this.indexOf(this.min());
-  };
-
-
-
